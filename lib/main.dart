40722a29ef601e7c441e88daae0ad5584d5d1019
PusulaAI77 <aleyna.erkulll@gmail.com>
2025-08-07 01:23:25 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(home: Scaffold(body: Center(child: Text('HELLOO !')))),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 2, 16, 31),
+                const Color.fromARGB(255, 4, 37, 69),
+                const Color.fromARGB(255, 8, 67, 126),
+                const Color.fromARGB(255, 54, 91, 127)
+              ],
+            ),
+          ),
+          child: const Center(child: Text('HELLOO !')),
+        ),
+      ),
+    ),
   );
 }
