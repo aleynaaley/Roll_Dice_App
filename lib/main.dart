0d3b473aa38414db63b1c37306cd4ead071acaaa
PusulaAI77 <aleyna.erkulll@gmail.com>
2025-08-10 01:30:23 +0300

    Added widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 2, 16, 31),
-                const Color.fromARGB(255, 4, 37, 69),
-                const Color.fromARGB(255, 8, 67, 126),
-                const Color.fromARGB(255, 54, 91, 127)
-              ],
-            ),
-          ),
-          child: const Center(child: Text('HELLOO !')),
-        ),
+        body: GradientSContainer(),
       ),
     ),
   );
+}
+
+class GradientSContainer extends StatelessWidget {
+  const GradientSContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 2, 16, 31),
+            const Color.fromARGB(255, 4, 37, 69),
+            const Color.fromARGB(255, 8, 67, 126),
+            const Color.fromARGB(255, 54, 91, 127),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'HELLOO !',
+          style: TextStyle(color: Colors.grey, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
