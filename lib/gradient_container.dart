15cbff513995e96c320e9b66a86dd8a16aa59218
PusulaAI77 <aleyna.erkulll@gmail.com>
2025-08-16 00:42:30 +0300

    changed variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:rool_dice_app/styled_text.dart';
 
+const startAligment = Alignment.topLeft;        //(const )final sabit , var değiştirilebilir olarak kullanılıyor
+const endAligment = Alignment.bottomRight;
+
 class GradientSContainer extends StatelessWidget {
   const GradientSContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            const Color.fromARGB(255, 2, 16, 31),
-            const Color.fromARGB(255, 4, 37, 69),
-            const Color.fromARGB(255, 8, 67, 126),
-            const Color.fromARGB(255, 54, 91, 127),
+             Color.fromARGB(255, 2, 16, 31),
+             Color.fromARGB(255, 4, 37, 69),
+             Color.fromARGB(255, 8, 67, 126),
+             Color.fromARGB(255, 54, 91, 127),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAligment,
+          end: endAligment,
         ),
       ),
-      child: const Center(
-        child:StyledText()
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
